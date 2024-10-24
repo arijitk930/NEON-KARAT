8dc3f0ce8fe0f8935037959a35580e09b96ae6d8
arijitk930 <arijitk930@gmail.com>
2024-10-24 13:15:01 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Features from './components/Features';
-import Footer from './components/Footer';
-import './App.css'
-import Testimonials from './components/Testimonials';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import "./App.css";
+import Testimonials from "./components/Testimonials";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
       <Features />
-      <Testimonials/>
+      <Testimonials />
       <Footer />
     </div>
   );
