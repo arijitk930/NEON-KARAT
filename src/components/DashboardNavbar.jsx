3c2bd43942ca7f803e813e652864225d6c8b2602
Mapur2 <rupammodak1@gmail.com>
2024-11-15 21:28:06 +0530

    Nav

--- src/components/DashboardNavbar.jsx
+++ src/components/DashboardNavbar.jsx
@@ -1,25 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="w-64 bg-gray-100 h-full border-r border-gray-300 flex flex-col">
       <div className="p-5 text-2xl font-bold">☰</div>
       <nav className="flex flex-col space-y-2 p-5">
-        <a href="#" className="p-3 rounded hover:bg-gray-200">
+        <Link to="/dashboard" className="p-3 rounded hover:bg-gray-200">
           Home
-        </a>
-        <a href="#" className="p-3 rounded hover:bg-gray-200">
+        </Link>
+        <Link to="/profileInsights" className="p-3 rounded hover:bg-gray-200">
           Insights
-        </a>
-        <a href="#" className="p-3 rounded hover:bg-gray-200">
+        </Link>
+        <Link to="#" className="p-3 rounded hover:bg-gray-200">
           Referrals
-        </a>
-        <a href="#" className="p-3 rounded hover:bg-gray-200">
+        </Link>
+        <Link to="#" className="p-3 rounded hover:bg-gray-200">
           Settings
-        </a>
-        <a href="#" className="p-3 rounded hover:bg-gray-200">
+        </Link>
+        <Link to="#" className="p-3 rounded hover:bg-gray-200">
           Account Details
-        </a>
+        </Link>
       </nav>
     </div>
   );
