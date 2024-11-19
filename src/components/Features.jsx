6ece2c911620c661531f54fc6e4626bbae0efdf0
arijitk930 <arijitk930@gmail.com>
2024-11-19 15:00:54 +0530

    added router paths

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,14 +1,18 @@
+import { Link, useLocation } from "react-router-dom";
+
 const Features = () => {
   const features = [
     {
       title: "Credit Card",
       description: "Custom credit cards tailored to creators.",
       icon: "💳",
+      path: "/credit-card",
     },
     {
       title: "Insights",
       description: "Get financial insights for your creator business.",
       icon: "📊",
+      path: "/insights",
     },
   ];
 
@@ -16,7 +20,7 @@ const Features = () => {
     <section className=" ">
       <div className="container mx-auto px-4">
         <div className="text-center mb-12">
-          <h2 className="text-4xl font-extrabold  font-primaryBold ">
+          <h2 className="text-4xl font-extrabold  font-primaryBold bg-[url('https://cdn.prod.website-files.com/62c48d78ef34931f8a604ef5/62c4904a072d7e734d9f4a1b_gradient.png')] text-transparent bg-clip-text bg-cover bg-center inline-block custom-stroke">
             OUR FEATURES
           </h2>
           <p className="text-lg text-gray-600 mt-4">
@@ -25,7 +29,8 @@ const Features = () => {
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8">
           {features.map((feature, idx) => (
-            <div
+            <Link
+              to={feature.path}
               key={idx}
               className="p-8 border border-gray-300 rounded-lg text-center shadow-lg bg-white transition-transform transform hover:scale-105 hover:shadow-xl cursor-pointer"
             >
@@ -34,7 +39,7 @@ const Features = () => {
                 {feature.title}
               </h3>
               <p className="text-gray-600">{feature.description}</p>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
